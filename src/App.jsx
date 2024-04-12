78d87aa958e5f22f0873e256f25a67a3d0d192db
vel <r.velmurugan1506@gmail.com>
2024-04-12 09:39:05 +0530

    added Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <main className="h-screen my-8">
+      <Sidebar/>
+    </main>
   );
 }
 
